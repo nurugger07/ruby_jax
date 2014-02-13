e088e956f854bbaec6e1210506ca64f75c65ce96
nurugger07 <dev@hashrocket.com>
2014-02-13 16:24:01 -0500

    add worker to supervisor

--- lib/ruby_jax/supervisor.ex
+++ lib/ruby_jax/supervisor.ex
@@ -5,14 +5,8 @@ defmodule RubyJax.Supervisor do
     :supervisor.start_link(__MODULE__, [])
   end
 
-  def init([]) do
-    children = [
-      # Define workers and child supervisors to be supervised
-      # worker(RubyJax.Worker, [])
-    ]
-
-    # See http://elixir-lang.org/docs/stable/Supervisor.Behaviour.html
-    # for other strategies and supported options
+  def init(list) do
+    children = [ worker(ListServer, list) ]
     supervise(children, strategy: :one_for_one)
   end
 end
