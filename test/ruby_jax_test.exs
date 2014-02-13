bc405165b669013d7df9c7c1abf893a79add11b0
nurugger07 <dev@hashrocket.com>
2014-02-13 14:12:38 -0500

    Add server tests

--- test/ruby_jax_test.exs
+++ test/ruby_jax_test.exs
@@ -1,7 +1,13 @@
 defmodule RubyJaxTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test :start_with_empty do
   end
+
+  test :add_items_to_list do
+  end
+
+  test :remove_items_from_list do
+  end
+
 end
