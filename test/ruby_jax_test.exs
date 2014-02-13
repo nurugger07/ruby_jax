4092ef5cdc5fe546fc66aff4563c4f8762ce468f
nurugger07 <dev@hashrocket.com>
2014-02-13 14:28:38 -0500

    implement tests

--- test/ruby_jax_test.exs
+++ test/ruby_jax_test.exs
@@ -1,13 +1,23 @@
 defmodule RubyJaxTest do
   use ExUnit.Case
 
-  test :start_with_empty do
+  test :clear_list do
+    list = ListServer.start_link
+    assert ListServer.items(list) == []
   end
 
   test :add_items_to_list do
+    list = ListServer.start_link
+    ListServer.add(list, "books")
+    assert ["books"] == ListServer.items(list)
   end
 
   test :remove_items_from_list do
+    list = ListServer.start_link
+    ListServer.add(list, "books")
+    ListServer.add(list, "magazines")
+    ListServer.remove(list, "books")
+    assert ["magazines"] == ListServer.items(list)
   end
 
 end
